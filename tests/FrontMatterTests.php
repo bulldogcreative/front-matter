b658d6c8463d7592e44088dbbee81e6c95eb38c1
Levi Durfee <levi.durfee@gmail.com>
2018-09-11 09:02:42 -0400

    Add new test

--- tests/FrontMatterTests.php
+++ tests/FrontMatterTests.php
@@ -9,6 +9,17 @@ class FrontMatterTests extends TestCase
     {
         $fm = FrontMatter::load('file.yaml');
         $this->assertEquals($fm->config()['title'], "Hello World");
+    }
+
+    public function testHtml()
+    {
+        $fm = FrontMatter::load('file.yaml');
+        $this->assertEquals($fm->html(), '<h1>Hello World</h1><p>It is i, levi.</p><p><a href="https://google.com">test</a></p>');
+    }
+
+    public function testBadYaml()
+    {
+        $fm = FrontMatter::load('tests/bad.yaml');
         $this->assertEquals($fm->html(), '<h1>Hello World</h1><p>It is i, levi.</p><p><a href="https://google.com">test</a></p>');
     }
 }
